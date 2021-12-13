7b5ae8afaf37c7602b6dc8dc12422dfdafe2fdee
Danny Oh <dannyoh0316@gmail.com>
2021-12-12 22:46:49 -0600

    check for valid recipe url

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from src.recipe import Recipe
 
 
+def get_recipe_url() -> str:
+    while True:
+        print('Please enter recipe URL:', end=' ')
+        recipe_url = input()
+        if 'allrecipes.com/recipe/' in recipe_url:
+            return recipe_url
+        print('Not a valid recipe URL.')
+
+
 def main():
-    print('Please enter recipe URL:', end=' ')
-    recipe_url = input()
+    recipe_url = get_recipe_url()
     recipe = Recipe(recipe_url=recipe_url)
-    print(recipe)
+    print(f'\n{recipe}')
 
 
 if __name__ == '__main__':
