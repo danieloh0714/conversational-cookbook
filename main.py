a28360ac3219a8540dc7668c0b66a3c41ac8993f
Danny Oh <dannyoh0316@gmail.com>
2021-12-25 01:05:21 -0600

    format files with black

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from src.recipe import Recipe
 
 def get_recipe_url() -> str:
     while True:
-        print('Please enter recipe URL:', end=' ')
+        print("Please enter recipe URL:", end=" ")
         recipe_url = input()
-        if re.search('allrecipes.com/recipe/[0-9]+/.+', recipe_url):
+        if re.search("allrecipes.com/recipe/[0-9]+/.+", recipe_url):
             return recipe_url
-        print('Not a valid recipe URL.')
+        print("Not a valid recipe URL.")
 
 
 def main():
     recipe_url = get_recipe_url()
     recipe = Recipe(recipe_url=recipe_url)
-    print(f'\n{recipe}')
+    print(f"\n{recipe}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/recipe.py
+++ src/recipe.py
@@ -3,9 +3,8 @@ from bs4 import BeautifulSoup
 
 
 class Recipe:
-
     def __init__(self, recipe_url: str):
-        self.title = ''
+        self.title = ""
         self.overview = dict()
         self.ingredients = []
         self.instructions = dict()
@@ -13,36 +12,39 @@ class Recipe:
 
     def parse_recipe_url(self, recipe_url: str) -> None:
         req = requests.get(recipe_url)
-        soup = BeautifulSoup(req.content, 'html.parser')
+        soup = BeautifulSoup(req.content, "html.parser")
 
-        title = soup.find('h1', class_='headline').getText()
+        title = soup.find("h1", class_="headline").getText()
         self.title = title
 
-        overview_divs = soup.find_all('div', class_='recipe-meta-item-header')
-        overview = {el.getText()[:-1]: el.next_sibling.next_sibling.getText()[:-1] for el in overview_divs}
+        overview_divs = soup.find_all("div", class_="recipe-meta-item-header")
+        overview = {
+            el.getText()[:-1]: el.next_sibling.next_sibling.getText()[:-1]
+            for el in overview_divs
+        }
         self.overview = overview
 
-        ingredient_divs = soup.find_all('span', class_='ingredients-item-name')
+        ingredient_divs = soup.find_all("span", class_="ingredients-item-name")
         ingredients = [el.getText() for el in ingredient_divs]
         self.ingredients = ingredients
 
-        instructions_divs = soup.find('ul', class_='instructions-section').find_all('p')
-        instructions = {i+1: el.getText() for i, el in enumerate(instructions_divs)}
+        instructions_divs = soup.find("ul", class_="instructions-section").find_all("p")
+        instructions = {i + 1: el.getText() for i, el in enumerate(instructions_divs)}
         self.instructions = instructions
 
     def __str__(self) -> str:
-        title = f'--- Recipe ---\n{self.title}'
+        title = f"--- Recipe ---\n{self.title}"
 
-        overview = '--- Overview ---'
+        overview = "--- Overview ---"
         for k, v in self.overview.items():
-            overview += f'\n{k}: {v}'
+            overview += f"\n{k}: {v}"
 
-        ingredients = '--- Ingredients ---'
+        ingredients = "--- Ingredients ---"
         for ingredient in self.ingredients:
-            ingredients += f'\n- {ingredient}'
+            ingredients += f"\n- {ingredient}"
 
-        instructions = '--- Instructions ---'
+        instructions = "--- Instructions ---"
         for step, instruction in self.instructions.items():
-            instructions += f'\n{step}. {instruction}'
+            instructions += f"\n{step}. {instruction}"
 
-        return f'{title}\n\n{overview}\n\n{ingredients}\n\n{instructions}'
+        return f"{title}\n\n{overview}\n\n{ingredients}\n\n{instructions}"
